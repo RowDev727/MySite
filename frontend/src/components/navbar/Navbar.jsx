fc09c5ee885e3f3af723fe6ff79064489d2f343a
RowDev727 <rowdev727@gmail.com>
2024-03-03 20:57:55 -0500

    Responsive Navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,25 +2,33 @@ import styles from './Navbar.module.css'
 import { NavLink } from 'react-router-dom'
 import { navLinks } from '../../data/navLinks'
 import { FaBars } from 'react-icons/fa'
+import { MdOutlineClose } from 'react-icons/md'
+import { useState } from 'react'
 
 const Navbar = () => {
-  return (
+    const [navShowing, setNavShowing] = useState(false)
+  
+    return (
     <nav>
         <div className={`${styles.navbarContainer} ${'container'}`}>
         <h1>AllStacks</h1>
-        <ul className={styles.navbarLinks}>
+        <ul className={`${styles.navbarLinks} ${navShowing ? `${styles.showNav}` : `${styles.hideNav}`}` }>
             {
                 navLinks.map(({name, path}, index) => {
                     return (
                         <li key={index}>
-                            <NavLink to={path}>{name}</NavLink>
+                            <NavLink to={path} className={({isActive}) => isActive ? `${styles.activeNav}` : ''}>{name}</NavLink>
                         </li>
                     )
                 })
             }
             <div className={`${styles.btnContainer}`}><NavLink className={styles.contactBtn} to={'/contact'}>Contact Us</NavLink></div>
         </ul>
-        <FaBars className={styles.bars}/>
+        <button className={styles.navToggleBtn} onClick={() => setNavShowing(!navShowing)}>
+            {
+                navShowing ? <MdOutlineClose /> : <FaBars />
+            }
+        </button>
         </div>
     </nav>
     
