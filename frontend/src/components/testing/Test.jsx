f5b3fe44108fc3e45d729f961501b481088d8f9f
RowDev727 <rowdev727@gmail.com>
2024-03-10 16:05:30 -0400

    Testing Stuff

--- frontend/src/components/testing/Test.jsx
+++ frontend/src/components/testing/Test.jsx
@@ -1,30 +1,69 @@
 import './Test.css'
 import { useState, useEffect } from 'react'
+import { ourTeam } from '../../data/ourTeam'
 
 function Test() {
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  // const [name, setName] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
 
   useEffect(() => {
-    fetch('api/message/1').then(res => res.json()).then(data => {
-      setName(data.user)
-      setEmail(data.email)
-      setMessage(data.message)
+    //fetchMessages()
+    fetch('api/messages').then(res => res.json()).then(data => {
+      setMessages(data.messages)
+      console.log(data.messages)
+      console.log(messages)
     })
   }, [])
 
+  const fetchMessages = async () => {
+    const response = await fetch('/api/messages')
+    const data = await response.json()
+    console.log(data)
+    console.log(data.messages)
+    setMessages(data.messages)
+    console.log(messages)
+  }
+  // useEffect(() => {
+  //   fetch('api/message/1').then(res => res.json()).then(data => {
+  //     setName(data.user)
+  //     setEmail(data.email)
+  //     setMessage(data.message)
+  //   })
+  // }, [])
+
   return (
     <div className="test-container">
-      <div className="test-container-header">
-        <h1>Contacts</h1>
+      <h1>Contacts</h1>
+      {
+        messages.map((message) => (
+          <p>{message.name}</p>
+        ))
+      }
+
+      
+      
+      {/* <div className="test-container-header">
+        <h1>Our Team!</h1>
       </div>
       <div className="test-container-body">
         <p>Name: {name}</p>
         <p>E-Mail: {email}</p>
         <p>Message: {message}</p>
-      </div>
+        {
+          ourTeam.map(({name, img, description}, index) => {
+              return (
+                  <div className="card">
+                    <p>Name: {name}</p>
+                    <p>Image: {img}</p>
+                    <p>Description: {description}</p>
+                  </div>
+              )
+          })
+        }
+      </div> */}
     </div>
   )
 }
---
+++ frontend/src/data/ourTeam.js
@@ -0,0 +1,16 @@
+export const ourTeam = [
+    {
+        name: "Chris",
+        img: "default.jpg",
+        description: "Chris is our lead developer and CEO!"
+    },
+    {
+        name: "John",
+        img: "default.jpg",
+        description: "John is our senior backend developer!"
+    },{
+        name: "Alex",
+        img: "default.jpg",
+        description: "Alex is our senior frontend developer!"
+    }
+]
