c88500d596ac4e97bb4334faa5e1317731c3e510
RowDev727 <rowdev727@gmail.com>
2024-03-10 12:25:35 -0400

    Hero: Fixed typing animation

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
     <div className={`${styles.heroContainer} ${styles.title} ${'container'}`}>
       <p className={`${styles.title1}`}>DEVELOP A WEB PRESENCE</p>
       <p className={`${styles.title2}`}>Grow your business.</p>
-      <p className={`${styles.title3}`}>Fast, Dynamic, Scalable & <span>Modern <ReactTyped strings={['Static Sites', 'Web Apps', 'Mobile Apps']} typeSpeed={120} backSpeed={140} loop/></span></p>
+      <p className={`${styles.title3}`}>Fast, Dynamic, Scalable & Modern</p>
+      <span><ReactTyped strings={['Static Sites', 'Web Apps', 'Mobile Apps']} typeSpeed={120} backSpeed={140} loop/></span>
       
       <p className={`${styles.title4}`}>Work closely with our experienced web developers to build tailored web applications for your organization.</p>
       <Link to={'/contact'} className={`${styles.getStartedBtn}`}>Get Started</Link>
