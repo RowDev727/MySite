f9ad371ba3d4a191bf3950ec3de134d9277076f1
RowDev727 <rowdev727@gmail.com>
2024-03-14 12:00:25 -0400

    Small change to FAQ style

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className={`${styles.heroContainer} ${styles.title} ${'container'}`}>
       <p className={`${styles.title1}`}>DEVELOP A WEB PRESENCE</p>
       <p className={`${styles.title2}`}>Grow your business.</p>
-      <p className={`${styles.title3}`}>Fast, Dynamic, Scalable & Modern</p>
+      <p className={`${styles.title3}`}>Dynamic, Scalable & Modern</p>
       <span><ReactTyped strings={['Static Sites', 'Web Apps', 'Mobile Apps']} typeSpeed={120} backSpeed={140} loop/></span>
       
       <p className={`${styles.title4}`}>Work closely with our experienced web developers to build tailored web applications for your organization.</p>
