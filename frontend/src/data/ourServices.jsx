c59c375d189ecddb4a3e305fc27f06b8565f9b67
RowDev727 <rowdev727@gmail.com>
2024-03-21 15:00:03 -0400

    Services Text Added

--- frontend/src/data/ourServices.jsx
+++ frontend/src/data/ourServices.jsx
@@ -9,41 +9,41 @@ export const ourServices = [
     {
         Icon: <FaLaptopCode />,
         title: "UI/UX Design",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "Our UI/UX designers specialize in creating elegant and intuitive user interfaces, ensuring a plesant user experience!"
     },
     {
         Icon: <HiCodeBracket />,
         title: "Web Development",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "Our team of developers specialize in working with the UI/UX team to build out designs tailored to the customers needs."
     },
     {
         Icon: <MdOutlinePhoneAndroid />,
         title: "Mobile Development",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "With mobile apps becoming the new norm, our developers can design, build and publish the mobile app of your dreams!"
     },
     {
         Icon: <AiFillApi />,
         title: "API Development",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "Our team of developers can design and build robust and secure REST API's using a variety of backend languages!"
     },
     {
         Icon: <BsDatabaseFill />,
         title: "Database Design",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "Our developers design and build database schema's for maximum efficiency, utilizing best practices to ensure security and speed."
     },
     {
         Icon: <MdShoppingCart />,
-        title: "Ecommerce",
-        points: "This is just some example text.  Still need to fill this in with something."
+        title: "E-commerce",
+        points: "Want to build your web presence?  An e-commerce site can provide your customers a safe and convenient way to buy your products."
     },
     {
         Icon: <GrVulnerability />,
         title: "Penetration Testing",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "Our certified ethical hackers can test the security of your site by looking for vulnerabilities and reporting their findings."
     },
     {
         Icon: <FaServer />,
         title: "Deployment & Hosting",
-        points: "This is just some example text.  Still need to fill this in with something."
+        points: "Once your site is built it must be deployed so that users around the world can see it.  We provide services to configure everything!"
     }
 ]
