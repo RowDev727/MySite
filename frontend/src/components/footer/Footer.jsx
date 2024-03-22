16cc7a8e66e0f44fd0da2a6aa862804d62527c24
RowDev727 <rowdev727@gmail.com>
2024-03-22 16:58:05 -0400

    Footer Responsive

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,46 +3,44 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={`${styles.footerContainer}`}>
-      <div className={`${styles.footerSection} container`}>
-        <div className={`${styles.col}`}>
-          <h1 className={`${styles.title}`}>RowdieDevs</h1>
-          <p className={`${styles.text}`}>Dedicated to providing our customers with exceptional software and exceptional service!</p>
+      <div className={`${styles.footer} container`}>
+        <div className={`${styles.footerHeader}`}>
+          <h1 className={`${styles.footerTitle}`}>RowdieDevs</h1>
+          <p className={`${styles.footerText}`}>Dedicated to providing our customers with exceptional software and exceptional service!</p>
         </div>
-        <div className={`${styles.col}`}>
-          <h3>Solutions</h3>
-          <ul>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Commerce</a></li>
-            <li><a href="#">Insight</a></li>
-          </ul>
-        </div>
-        <div className={`${styles.col}`}>
-          <h3>Support</h3>
+        <div className={`${styles.footerBody}`}>
+            <div className={`${styles.footerCard}`}>
+            <h3 className={`${styles.footerCardTitle}`}>Solutions</h3>
             <ul>
-              <li><a href="#">Pricing</a></li>
-              <li><a href="#">Documentation</a></li>
-              <li><a href="#">Guides</a></li>
-              <li><a href="#">API Status</a></li>
+                <li><a href="#">Analytics</a></li>
+                <li><a href="#">Marketing</a></li>
+                <li><a href="#">Commerce</a></li>
             </ul>
-        </div>
-        <div className={`${styles.col}`}>
-          <h3>Company</h3>
-            <ul>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Jobs</a></li>
-              <li><a href="#">Press</a></li>
-            </ul>
-        </div>
-        <div className={`${styles.col}`}>
-          <h3>Legal</h3>
-            <ul>
-              <li><a href="#">Claim</a></li>
-              <li><a href="#">Privacy</a></li>
-              <li><a href="#">Terms</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-            </ul>
+            </div>
+            <div className={`${styles.footerCard}`}>
+            <h3 className={`${styles.footerCardTitle}`}>Support</h3>
+                <ul>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Documentation</a></li>
+                <li><a href="#">Guides</a></li>
+                </ul>
+            </div>
+            <div className={`${styles.footerCard}`}>
+            <h3 className={`${styles.footerCardTitle}`}>Company</h3>
+                <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Jobs</a></li>
+                </ul>
+            </div>
+            <div className={`${styles.footerCard}`}>
+            <h3 className={`${styles.footerCardTitle}`}>Legal</h3>
+                <ul>
+                <li><a href="#">Claim</a></li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Terms</a></li>
+                </ul>
+            </div>
         </div>
       </div>
     </div>
