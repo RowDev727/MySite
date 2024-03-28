52c354a87ed49e50e1046329d1734e4c28c321b8
RowDev727 <rowdev727@gmail.com>
2024-03-28 13:06:40 -0400

    User model added

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,12 @@ from .extensions import db
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50), unique=True)
-    message = db.Column(db.String(255))
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    
+    def __repr__(self):
+        return f'<Message {self.name}, {self.email}, {self.message}>'
     
     def to_json(self):
         return {
@@ -14,12 +17,23 @@ class Message(db.Model):
             "message": self.message
         }
         
+        
 class NewsLetterContact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f'<NewsLetterContact {self.email}>'
+        
     def to_json(self):
         return {
             "id": self.id,
             "email": self.email
         }
+        
+        
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False)
